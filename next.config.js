b0765d2c41110c86abfd635c2aea2b89ff500b0f
chrene <chrene91@gmail.com>
2022-04-15 08:44:12 +0200

    add font face

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-  env: {
+
+const withFonts = require('next-fonts');
+
+module.exports = withFonts({
+   enableSvg: true,
+   webpack(config, options) {
+     return config;
+   },
+   env: {
     dxEnabled: process.env.DX_ENABLED
   },
-}
+}); 
