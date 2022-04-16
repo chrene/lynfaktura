473977fba624a78a5132b42441fb54633d0d45ea
chrene <chrene91@gmail.com>
2022-04-17 01:47:48 +0200

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import CreateInvoiceForm from '../components/create-invoice-form';
 import Meta from '../components/meta/meta';
 
